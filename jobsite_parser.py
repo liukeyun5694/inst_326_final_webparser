0448ea68b77feb3828833d89b8c4ad74b006e40d
liukeyun5694 <92655462+liukeyun5694@users.noreply.github.com>
2021-11-21 22:45:49 -0500

    Update jobsite_parser.py

--- jobsite_parser.py
+++ jobsite_parser.py
@@ -8,8 +8,9 @@ import csv
 
 for i in range(0,20,10):
     
-    url = requests.get(f"https://www.indeed.com/jobs?q=data%20analyst%20&l=Washington%2C%20DC&start=0").text
-    soup = BeautifulSoup(url, 'html.parser')
+    url = "https://www.indeed.com/jobs?q=data%20analyst%20&l=Washington%2C%20DC&start=0"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
  
 
     for url in soup.find_all('div',{"class":"job_seen_beacon"}):
@@ -17,8 +18,11 @@ for i in range(0,20,10):
         i = url.find('tbody')
         a = i.find('tr')
         
-
+        results = soup.find("Results")
+        
         for j in a.find_all('h2',{'class': 'jobTitle jobTitle-color-purple jobTitle-newJob'}):
             job_title = j.find_all('span')[1].get_text()
             print(job_title)
             
+            users_job = results.find_all(string = "")
+            print(users_job)
